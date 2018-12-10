cbc48665ab4d648d638a8f6a1afdc85d0aecffa8
GraceBa <45609466+GraceBa@users.noreply.github.com>
2018-12-11 10:34:47 +1100

    Create controlHammer.js

---
+++ Reverse/controlHammer.js
@@ -0,0 +1,53 @@
+        AFRAME.registerComponent("foo",{
+        init:function() {
+          var element = document.querySelector('body');
+          this.marker = document.getElementById('VisibleImage');
+          this.marker2 = document.getElementById('flipVisibleImage')'
+          var model = document.getElementById('dnaA');
+          var model2 = document.getElementById('dnaB');
+          this.markerVisible = false
+      var hammertime = new Hammer(element);
+      var pinch = new Hammer.Pinch();
+      hammertime.add(pinch); // add it to the Manager instance
+      
+      hammertime.on('pan', (ev) => {
+        if (!this.markerVisible) { return;}
+        let rotation = model.getAttribute("rotation")
+        switch(ev.direction) {
+          case 2:
+            rotation.y = rotation.y + 4
+            break;
+          case 4:
+            rotation.y = rotation.y - 4
+            break;
+          case 8:
+            rotation.x = rotation.x + 4
+            break;
+          case 16:
+            rotation.x = rotation.x - 4
+            break;
+          default:
+            break;
+        } 
+        model.setAttribute("rotation", rotation)
+		model2.setAttribute("rotation", rotation)
+      }); 
+      
+      hammertime.on("pinch", (ev) => {
+      	if (!this.markerVisible) { return; }
+		  let initialScale = model.getAttribute("scale")
+          let scale = {x:ev.scale, y:ev.scale, z:ev.scale}
+          model.setAttribute("scale", initialScale + scale);
+		  model2.setAttribute("scale", initialScale + scale);
+
+      });
+        },
+        tick:function() {
+          if (this.marker && this.marker.object3D.visible == true) {
+            this.markerVisible = true
+            console.log(this.markerVisible)
+          } else {
+            this.markerVisible = false
+        }
+        }
+      });
